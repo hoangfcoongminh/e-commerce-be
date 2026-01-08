e1d058eb517e9fcb01710373460c0a7136ae6c4a
hoangfcoongminh <hoangfcoongminh@gmail.com>
2026-01-08 15:51:57 +0700

    fix: register and login

--- src/main/java/com/edward/order/dto/request/RegisterRequest.java
+++ src/main/java/com/edward/order/dto/request/RegisterRequest.java
@@ -7,10 +7,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class RegisterRequest {
 
     @NotBlank(message = "Email is required")
